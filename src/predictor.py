2cebbe887c49eecf0af97232adbc002e5e3ccce8
Víctor González Fernández <vgonzf04@estudiantes.unileon.es>
2026-09-17 18:43:47 +0200

    funcionalidad corregida de predictor + guardar info del mejor modelo

--- src/predictor.py
+++ src/predictor.py
@@ -1,33 +1,70 @@
 import joblib
 from tensorflow import keras
 from data_loader import preprocessed_sklearn, preprocessed_for_prediction_nn
-from pandas import DataFrame
+import pandas as pd
 from pathlib import Path
 
 # threshold para marcar a partir de que prob es 0 o 1
 NN_THRESHOLD = 0.4 # move it to config.py
 # directorio de modelos guardados y entrenados
-MODELS_DIR = Path("models/tests")
+MODELS_DIR = Path("models")
+# directorio de info de mejor modelo
+METADATA_PATH = Path("models/best_model_metadata.json")
+# directorio de datos para predecir
+DATA_PATH = Path("data/raw/data_predict.csv")
 
-def sklearn_models_predict(reservation_df:DataFrame):
-    X_preprocessed_sklearn = preprocessed_sklearn(reservation_df)
 
-    lr_model = joblib.load(MODELS_DIR / "logistic_regression.pkl")
-    dt_model = joblib.load(MODELS_DIR / "decision_tree.pkl")
-    rf_model = joblib.load(MODELS_DIR / "random_forest.pkl")
-    xgb_model = joblib.load(MODELS_DIR / "xgboost.pkl")
+def sklearn_model_predict(df_predict:DataFrame):
+    # preprocesamiento de datos para modelos clasicos
+    X_preprocessed_sklearn = preprocessed_sklearn(df_predict)
 
-    y_pred_lr = lr_model.predict(X_preprocessed_sklearn)
-    y_pred_dt = dt_model.predict(X_preprocessed_sklearn)
-    y_pred_rf = rf_model.predict(X_preprocessed_sklearn)
-    y_pred_xgb = xgb_model.predict(X_preprocessed_sklearn)
+    # cargamos el modelo
+    best_model = joblib.load(MODELS_DIR / "best_model.pkl")
 
-    return y_pred_lr, y_pred_dt, y_pred_rf, y_pred_xgb
+    # calculamos precciones
+    y_pred = best_model.predict(X_preprocessed_sklearn)
 
-def neural_network_model_predict(reservation_df:DataFrame):
-    X_preprocessed_nn = preprocessed_for_prediction_nn(reservation_df)
-    nn_model = keras.models.load_model(MODELS_DIR / "neural_network.keras")
-    y_prob_nn = nn_model.predict(X_preprocessed_nn)
+    # devolvemos predicciones
+    return y_pred
+
+def neural_network_model_predict(df_predict:DataFrame):
+    # preprocesamiento de datos para red neuronal
+    X_preprocessed_nn = preprocessed_for_prediction_nn(df_predict)
+
+    # cargamos el modelo de red neuronal
+    best_model = keras.models.load_model(MODELS_DIR / "best_model.keras")
+
+    # predecimos, devuelve probabilidades
+    y_prob_nn = best_model.predict(X_preprocessed_nn)
+    # con las probabilidades y el threshold elegido durante el experimentacion calculamos prediciones
     y_pred_nn = (y_prob_nn > NN_THRESHOLD).astype(int)
 
-    return y_prob_nn, y_pred_nn
+    # devolvemos predicciones
+    return y_pred_nn
+
+
+def main():
+    # cargar datos para predictor
+    df_predict = pd.read(DATA_PATH)
+
+    # cargamos info del mejor modelo
+    with open(METADATA_PATH, "r") as file:
+        metadata = json.load(file)
+
+    # guardamos tipo de modelo 
+    best_model_type = metadata["best_model_type"]
+
+    # comparamos si mejor modelo es tipo clasico o red neuronal
+    if best_model_type == "sklearn":
+        # caso de modelo clasico
+        y_pred = sklearn_model_predict(df_predict)
+
+    else:
+        # caso de red neuronal
+        y_pred = neural_network_model_predict(df_predict)
+
+
+    # print para ver predicciones 
+
+if __name__ == "__main__":
+    main()
